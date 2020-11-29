966a163261861c7e4d0f12ce423cdd95ff921d63
Sirisha443 <75203551+Sirisha443@users.noreply.github.com>
2020-11-29 22:53:52 +0530

    Create lab 1.java

---
+++ lab 1.java
@@ -0,0 +1,36 @@
+
+
+   import java.util.*;
+   	class lab 1
+	{
+	public static void main(string arg[])
+	double r1,r2,d;
+	scanner sc=new scanner(system.in);
+	system.out.println("enter a value"):
+	    a=s.nextint();
+	system.out.println("enter b value"):
+	    b=s.nextint();
+	system.out.println("enter c value"):
+	    c=s.nextint();
+	system.out.println("given quadratic eqn is:"+a+"x^:+"+b"x+"+c);
+		d=b*b-u*a*c;
+		if(d>0)
+		{
+	system.out.println(roots are real and unequal");
+		r1=(-b+Math.sqr+(d))/(2*a);
+		r1=(-b-Math.sqr+(d))/(2*a);
+	system.out.println("first root is"+r1);
+	system.out.println("second root is"+r2);
+		}
+		else if(d==0)
+		{
+	system.out.println("roots are real and equal");
+		r1=(-b+Math.sqr(d))/(2*a);
+	system.out.println("root is"+r1);
+		}
+		else
+		{
+	system.out.println("roots are imaginary");
+		}
+	       }
+		
